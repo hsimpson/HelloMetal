4f74b69d185bfe8356468c426ddf2cf03ac39da7
Daniel Toplak <homer.simpson123@gmx.de>
2022-01-11 01:50:20 +0100

    feat: render 1st triangle

--- HelloMetal/ViewController.swift
+++ HelloMetal/ViewController.swift
@@ -5,14 +5,38 @@
 //  Created by Daniel Toplak on 10.01.22.
 //
 
+import Metal
+import MetalKit
 import Cocoa
 
 class ViewController: NSViewController {
 
+    var mtkView: MTKView!
+    var renderer: Renderer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let mtkViewTemp = self.view as? MTKView else {
+            print("View attached to ViewController is not an MTKView!")
+            return
+        }
+        mtkView = mtkViewTemp
+        
+        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+            print("Metal is not supported on this device")
+            return
+        }
+        
+        print("My GPU is: \(defaultDevice)")
+        mtkView.device = defaultDevice
+        
+        guard let tempRenderer = Renderer(mtkView: mtkView) else {
+            print("Render failed to initalize")
+            return
+        }
+        renderer = tempRenderer
+        mtkView.delegate = renderer
     }
 
     override var representedObject: Any? {
